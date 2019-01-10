2cb6e8c92a982aaf084351514ac2e8f3570dbd87
Sharaf Zaman <sharafzaz121@gmail.com>
2019-01-11 05:19:17 +0530

    `read_file` function added for lighttouch-base

--- fs.lua
+++ fs.lua
@@ -126,4 +126,17 @@ function fs.mklink(src, dest)
   return fs.symlink(src, dest)
 end
 
+
+function fs.read_file(path)
+  local file = io.open(path, "r")
+
+  if not file then
+    return nil
+  end
+
+  local file_content = file:read("*all")
+  file:close()
+  return file_content
+end
+
 return fs
